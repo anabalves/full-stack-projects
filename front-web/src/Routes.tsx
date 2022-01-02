1bc64db37fd1cf3dcae94ea874d9232c716d281e
anabalves <ana.alves41@fatec.sp.gov.br>
2022-01-02 13:50:41 -0300

    Add Navbar

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -1,6 +1,19 @@
+import { BrowserRouter, Routes as Router, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
 function Routes() {
   return (
-    <h1>Rotas</h1>
+    <>
+      <BrowserRouter>
+      <Navbar />
+      <Router>
+        <Route path="/" element={<Home />} />
+      </Router>
+      <Footer />
+    </BrowserRouter>
+    </>
   );
 }
 
