1bf3381ba0db2e46f3652493580309ca282775ac
anabalves <ana.alves41@fatec.sp.gov.br>
2022-01-14 09:15:57 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,23 +1,29 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import MovieCard from "../../components/MovieCard";
 import Pagination from "../../components/Pagination";
+import { MoviePage } from "../../types/movie";
 import { BASE_URL } from "../../utils/request";
 
 function Listing() {
-  // Forma errada
+  const [pageNumber, setPageNumber] = useState(0);
 
-  axios
-    .get(`${BASE_URL}/movies?page=0&size=10&sort=title,asc`)
-    .then((response) => {
-      console.log(response.data);
-    })
-    .catch(() => {
-      toast.error("Erro ao listar filmes.");
-    });
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/movies?page=3&size=10&sort=title,asc`)
+      .then((response) => {
+        const data = response.data as MoviePage;
+        setPageNumber(data.number);
+      })
+      .catch(() => {
+        toast.error("Erro ao listar filmes.");
+      });
+  }, []);
 
   return (
     <>
+      <p>{pageNumber}</p>
       <Pagination />
       <div className="container">
         <div className="row">
