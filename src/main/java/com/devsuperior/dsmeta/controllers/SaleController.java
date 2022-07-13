72d1b06b4d1e82ad2dda4dfb90c0f921d62bde5f
anabalves <ana.alves41@fatec.sp.gov.br>
2022-07-13 17:50:10 -0300

    Date select

--- src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -5,11 +5,12 @@ import com.devsuperior.dsmeta.services.SaleService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -21,8 +22,11 @@ public class SaleController {
 
     @GetMapping
     @ApiOperation(value = "Fetch all sales details", notes = "get all sales")
-    public List<Sale> findSales() {
-        return service.findSales();
+    public Page<Sale> findSales(
+            @RequestParam(value="minDate", defaultValue = "") String minDate,
+            @RequestParam(value="maxDate", defaultValue = "") String maxDate,
+            Pageable pageable) {
+        return service.findSales(minDate, maxDate, pageable);
     }
 
 }
